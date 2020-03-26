2a3eb43978c379e1aa392d4e23796eb58565a6ac
ALFsarah <62641547+ALFsarah@users.noreply.github.com>
2020-03-26 09:26:44 +0100

    Update oscillationController.py

--- oscillationController.py
+++ oscillationController.py
@@ -19,35 +19,28 @@ class controller(Sofa.PythonScriptController):
 
     def onBeginAnimationStep(self, dt):
             self.dt = self.node.findData('dt').value
-            incr = self.dt*1000.0;
             start=time.time()
             self.totalTime = self.totalTime +dt
-           # print(self.totalTime)
             print("The time is " + str(self.pressureConstraint1Node.findData('time')))
             #period
             B=2
-
-            self.MecaObject1=self.cube1Node.getObject('tetras');
-
+            
             self.pressureConstraint1 = self.pressureConstraint1Node.getObject('SurfacePressureConstraint')
-
+            
             if (math.sin(B*self.totalTime) >= 0):
-
                 pressureValue = self.pressureConstraint1.findData('value').value[0][0] + 0.001
                 if pressureValue > 1.5:
                     pressureValue = 1.5
-                self.pressureConstraint1.findData('value').value = str(pressureValue)
-                print("The value is " + str(self.pressureConstraint1.findData('value').value[0][0]))
-                print("The volume is " + str((self.pressureConstraint1.findData('cavityVolume').value)))
+               
 
             if (math.sin(B*self.totalTime) < 0):
 
                 pressureValue = self.pressureConstraint1.findData('value').value[0][0] - 0.001
                 if pressureValue < 0:
                     pressureValue = 0
-                self.pressureConstraint1.findData('value').value = str(pressureValue)
-                print("The value is " + str(self.pressureConstraint1.findData('value').value[0][0]))
-                print("The volume is " + str((self.pressureConstraint1.findData('cavityVolume').value)))
+            self.pressureConstraint1.findData('value').value = str(pressureValue)
+            print("The value is " + str(self.pressureConstraint1.findData('value').value[0][0]))
+            print("The volume is " + str((self.pressureConstraint1.findData('cavityVolume').value)))
                 
 
 
